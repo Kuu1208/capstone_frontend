6b868d937f95e406c478472e63f0a1d9d04a4418
Kuu1208 <130426876+Kuu1208@users.noreply.github.com>
2024-05-13 16:24:54 +0900

    경로 수정

--- src/components/SecondScreen.jsx
+++ src/components/SecondScreen.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import Slider from "react-slick";
+import { Link } from 'react-router-dom';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import './SecondScreen.css';
@@ -126,6 +127,7 @@ const SecondScreen = () => {
             .catch(error => console.error('Error fetching blog choices:', error));
     }, []);
 
+
     const settings = {
         dots: true,
         infinite: true,
@@ -152,93 +154,119 @@ const SecondScreen = () => {
 
     return (
         <div className="second-screen">
-            <h2>⭐ 오늘의 베스트셀러 </h2>
+            <h2>🏅 오늘의 베스트셀러 </h2>
             <Slider {...settings}>
-                {bestsellers.map((bestseller, index) => (
-                    <div key={index} className="bestseller-item">
-                        <div className="rank">{bestseller.bestRank}</div>
-                        <img src={bestseller.cover} alt={bestseller.title} className="bestseller-image" />
-                        <p className="title">{bestseller.title}</p>
-                        <p className="author">{bestseller.author}</p>
-                    </div>
-                ))}
+            {bestsellers.map((bestseller, index) => (
+            <div key={index} className="bestseller-item">
+            <div className="rank">{bestseller.bestRank}</div>
+            <img src={bestseller.cover} alt={bestseller.title} className="bestseller-image" onClick={(e) => e.stopPropagation()} /> 
+            <Link to={`/book/${bestseller.id}`} className="book-link">
+                <p className="title">{bestseller.title}</p> {/* 제목 클릭 가능 */}
+            </Link>
+            <p className="author" onClick={(e) => e.stopPropagation()}>{bestseller.author}</p> {/* 저자 클릭 이벤트 비활성화 */}
+            </div>
+            ))}
             </Slider>
+
             <h2>🆕 신간 베스트셀러</h2>
             <Slider {...settings}>
-                {newBooks.map((book, index) => (
-                    <div key={index} className="new-book-item">
-                        <img src={book.cover} alt={book.title} className="new-book-image" />
-                        <p className="title01">{book.title}</p>
-                        <p className="author01">{book.author}</p>
-                    </div>
-                ))}
+            {newBooks.map((book, index) => (
+            <div key={index} className="new-book-item">
+            <img src={book.cover} alt={book.title} className="new-book-image" onClick={(e) => e.stopPropagation()} />
+            <Link to={`/book/${book.id}`} className="book-link">
+                <p className="title01">{book.title}</p>
+            </Link>
+            <p className="author" onClick={(e) => e.stopPropagation()}>{book.author}</p>
+            </div>
+            ))}
             </Slider>
+
+
             <h2>✔ Blog Choice 베스트셀러</h2>
             <Slider {...settings}>
-                {blogChoices.map((blogChoice, index) => (
-                    <div key={index} className="blog-choice-item">
-                        <div className="rank01">{blogChoice.bestRank}</div>
-                        <img src={blogChoice.cover} alt={blogChoice.title} className="blog-choice-image" />
-                        <p className="title02">{blogChoice.title}</p>
-                        <p className="author02">{blogChoice.author}</p>
-                    </div>
-                ))}
+            {blogChoices.map((blogChoice, index) => (
+            <div key={index} className="blog-choice-item">
+            <div className="rank">{blogChoice.bestRank}</div>
+            <img src={blogChoice.cover} alt={blogChoice.title} className="blog-choice-image" onClick={(e) => e.stopPropagation()} />
+            <Link to={`/book/${blogChoice.id}`} className="book-link">
+                <p className="title02">{blogChoice.title}</p>
+            </Link>
+            <p className="author02" onClick={(e) => e.stopPropagation()}>{blogChoice.author}</p>
+            </div>
+            ))}
             </Slider>
+
+
             <h2>📔경영경제 베스트셀러</h2>
             <Slider {...settings}>
                 {economicBooks.map((economicBooks, index) => (
-                    <div key={index} className="economic-books-item">
-                        <div className="rank02">{economicBooks.bestRank}</div>
-                        <img src={economicBooks.cover} alt={economicBooks.title} className="econmoic-books-image" />
+                    <div key = {index} className="economic-books-item">
+                        <div className="rank">{economicBooks.bestRank}</div>
+                        <img src = {economicBooks.cover} alt = {economicBooks.title} className="econmoic-books-image"onClick={(e) => e.stopPropagation()} />
+                        <Link to ={`/book/${economicBooks.id}`} className="book-link">
                         <p className="title03">{economicBooks.title}</p>
-                        <p className="author03">{economicBooks.author}</p>
-                    </div>
+                        </Link>
+                        <p className="author03" onClick={(e) => e.stopPropagation()}>{economicBooks.author}</p>
+                        </div>
                 ))}
-            </Slider>
+                </Slider>
+
             <h2>📚 문학 베스트셀러</h2>
             <Slider {...settings}>
                 {literature.map((literature, index) => (
-                    <div key={index} className="literature-books-item">
-                        <div className="rank03">{literature.bestRank}</div>
-                        <img src={literature.cover} alt={literature.title} className="literature-books-image" />
+                    <div key = {index} className="literature-books-item">
+                        <div className="rank">{literature.bestRank}</div>
+                        <img src = {literature.cover} alt={literature.title} className="literature-books-image"onClick={(e) => e.stopPropagation()} />
+                        <Link to ={`/book/${literature.id}`} className="book-link">
                         <p className="title04">{literature.title}</p>
-                        <p className="author04">{literature.author}</p>
+                        </Link>
+                        <p className="author04" onClick={(e) => e.stopPropagation()}>{literature.author}</p>
                     </div>
-                ))}
-            </Slider>
+                    ))}
+                    </Slider>
+
+
             <h2>🧪 과학 베스트셀러</h2>
             <Slider {...settings}>
                 {science.map((science, index) => (
-                    <div key={index} className="science-books-item">
-                        <div className="rank03">{science.bestRank}</div>
-                        <img src={science.cover} alt={science.title} className="science-books-image" />
+                    <div key = {index} className="science-books-item">
+                        <div className="rank">{science.bestRank}</div>
+                        <img src = {science.cover} alt={science.title} className="science-books-image"onClick={(e) => e.stopPropagation()} />
+                        <Link to ={`/book/${science.id}`} className="book-link">
                         <p className="title04">{science.title}</p>
-                        <p className="author04">{science.author}</p>
+                        </Link>
+                        <p className="author04" onClick={(e) => e.stopPropagation()}>{science.author}</p>
                     </div>
                 ))}
-            </Slider>
+                </Slider>
+
             <h2>👥 인문 베스트셀러</h2>
             <Slider {...settings}>
                 {humanities.map((humanities, index) => (
-                    <div key={index} className="humanities-books-item">
-                        <div className="rank03">{humanities.bestRank}</div>
-                        <img src={humanities.cover} alt={humanities.title} className="humanities-books-image" />
-                        <p className="title04">{humanities.title}</p>
-                        <p className="author04">{humanities.author}</p>
+                    <div key = {index} className="humanities-books-item">
+                        <div className = "rank">{humanities.bestRank}</div>
+                        <img src = {humanities.cover} alt={humanities.title} className="humanities-books-image"onClick={(e) => e.stopPropagation()} />
+                        <Link to ={`/book/${humanities.id}`} className="book-link">
+                            <p className="title04">{humanities.title}</p>
+                            </Link>
+                            <p className="author04" onClick={(e) => e.stopPropagation()}>{humanities.author}</p>
                     </div>
                 ))}
-            </Slider>
+                </Slider>
+
             <h2>💪 자기계발 베스트셀러</h2>
             <Slider {...settings}>
                 {improve.map((improve, index) => (
-                    <div key={index} className="improve-books-item">
-                        <div className="rank03">{improve.bestRank}</div>
-                        <img src={improve.cover} alt={improve.title} className="improve-books-image" />
+                    <div key = {index} className="improve-books-item">
+                        <div className = "rank">{improve.bestRank}</div>
+                        <img src = {improve.cover} alt={improve.title} className="improve-books-image"onClick={(e) => e.stopPropagation()} />
+                        <Link to ={`/book/${improve.id}`} className="book-link">
                         <p className="title04">{improve.title}</p>
-                        <p className="author04">{improve.author}</p>
-                    </div>
+                        </Link>
+                        <p className="author04" onClick={(e) => e.stopPropagation()}>{improve.author}</p>
+                        </div>
                 ))}
-            </Slider>
+                </Slider>
         </div>
     );
 }
